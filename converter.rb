2accc4c4932764f9dd93dac3fea13a742a7107e4
Uiri <uiri00@gmail.com>
2010-10-20 19:11:10 -0400

    updated converter

--- converter.rb
+++ converter.rb
@@ -1,21 +1,20 @@
-puts 'Decimal or binary?'
+puts 'Decimal or binary'
 a = gets.chomp
-
-while a != 'decimal'
-  if a != 'binary'
-    puts 'Decimal or binary?'
+while a != 'end'
+  if a == 'binary'
+    puts 'Enter a binary number'
+    b = gets.chomp
+    puts b.to_i(2)
+    puts 'Decimal or binary'
+    a = gets.chomp
+  elsif a == 'decimal'
+    puts 'Enter a decimal number'
+    b = gets.chomp
+    puts b.to_i.to_s(2)
+    puts 'Decimal or binary'
     a = gets.chomp
   else
-    break
+    puts 'Decimal or binary'
+    a = gets.chomp
   end
-end
-
-if a == 'decimal'
-  puts 'Enter a decimal number'
-  b = gets.chomp
-  puts b.to_i.to_s(2)
-else
-  puts 'Enter a binary number'
-  b = gets.chomp
-  puts b.to_i(2)
 end

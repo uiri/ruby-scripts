081f406886d192c0d3e2dd2a0b4d869ca70ab5f4
Uiri <uiri00@gmail.com>
2010-10-26 20:00:24 -0400

    added GPL copyright stuff and license.

--- chatGUI.rb
+++ chatGUI.rb
@@ -12,6 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You may contact Uiri Noyb via electronic mail with the address uiri@compucrunch.com
 
 require 'gtk2'
 

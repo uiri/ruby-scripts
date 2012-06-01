1df6ce36a408f627c3dfd270e7c7670219f0c7b6
Uiri Noyb <uiri00@gmail.com>
2012-06-01 17:43:02 -0400

    Rickroll code golf attempt

---
+++ rickroll.rb
@@ -0,0 +1,35 @@
+ng="Never gonna"
+n="(Ooh)
+"+ng+" give, never gonna give
+(Give you up)\n"
+j = "I just wanna tell you how I'm feeling
+Gotta make you understand\n\n"
+c = ng+" give you up
+"+ng+" let you down
+"+ng+" run around and desert you
+"+ng+" make you cry
+"+ng+" say goodbye
+"+ng+" tell a lie and hurt you\n\n"
+def d(c)
+2.times{|i|puts c}end
+v="We've known each other for so long
+Your heart's been aching but
+You're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it"
+puts "We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+"+j
+puts c
+puts v
+puts "And if you ask me how I'm feeling
+Don't tell me you're too blind to see\n\n"
+d(c)
+2.times{|i|puts "(Ooh, give you up)\n"}
+2.times{|i|puts n}
+puts "\n"+v
+puts "\n"+j
+d(c)
+puts c.chop
